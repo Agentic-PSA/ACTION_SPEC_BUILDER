c639c8595ac5002fab617b6b71e64aafa2cc3d07
Robert J <robert.jalinski@agentic.pl>
2025-11-03 14:23:59 +0100

    dodawanie kategorii

--- src/services/db_service.py
+++ src/services/db_service.py
@@ -3,6 +3,7 @@ import os
 import json
 import psycopg2
 from psycopg2.extras import RealDictCursor
+import requests
 
 def category_to_type(type, category):
     # połączenie
@@ -79,4 +80,66 @@ def get_category_by_id(category_id):
             cur.execute(query, (str(category_id),))
             category = cur.fetchone()
 
-    return category
+        if category:
+            return category
+
+        category_from_api = get_category_tree(category_id)
+        if not category_from_api:
+            return None
+
+        categories = {item["categoryId"]: item for item in category_from_api}
+        rows = []
+        for cat in category_from_api:
+            path = []
+            current = cat
+            while current:
+                path.append(current)
+                parent_id = current["parentCategoryId"]
+                current = categories.get(parent_id)
+            path.reverse()
+
+            row = {}
+            for i, level in enumerate(path, start=1):
+                row[f"categoryid_level{i}"] = level["categoryId"]
+                row[f"categoryname_level{i}"] = level["name"]
+            rows.append(row)
+
+        row = next((r for r in rows if r.get("categoryid_level3") == category_id), None)
+        if not row:
+            return None
+
+        query_insert = """
+            INSERT INTO iserwis_categories (
+                categoryid_level1, categoryname_level1,
+                categoryid_level2, categoryname_level2,
+                categoryid_level3, categoryname_level3,
+                update_date
+            )
+            VALUES(%s, %s, %s, %s, %s, %s, now())
+            ON CONFLICT (categoryid_level3) DO NOTHING
+            RETURNING *;
+        """
+        with conn.cursor(cursor_factory=RealDictCursor) as cur:
+            cur.execute(
+                query_insert, (
+                    row.get("categoryid_level1"),
+                    row.get("categoryname_level1"),
+                    row.get("categoryid_level2"),
+                    row.get("categoryname_level2"),
+                    row.get("categoryid_level3"),
+                    row.get("categoryname_level3")
+                )
+            )
+            inserted = cur.fetchone()
+            conn.commit()
+
+        return inserted or row
+
+def get_category_tree(category_id: int):
+    url = f"https://api-int-dev.action.pl/api/1.0/Pim/Categories/{category_id}/Hierarchy"
+    headers = {
+        'ApiKey': 'aksdgjhKSJFHLkjdsfhfkdjhKLJSHDFf'
+    }
+
+    response = requests.get(url, headers=headers)
+    return response.json().get("data")
